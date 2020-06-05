2d5292210b2a1d5544761a83dfddd6feefcd6a2f
TNORTHERN <traynorthern@yahoo.com>
2020-06-05 08:28:36 -0500

    more image optimization

--- components/portfolio/about/AboutDescription.vue
+++ components/portfolio/about/AboutDescription.vue
@@ -10,7 +10,7 @@
         data-aos-duration="350"
       >
         <img
-          src="https://i.imgur.com/krZgJMr.jpg"
+          src="https://i.imgur.com/4XJ2JwZ.jpg"
           alt="person typing"
           class="about__description-image"
         >
